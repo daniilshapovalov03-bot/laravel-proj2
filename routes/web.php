14abd9635418c0c188145b32079b551f715a8c00
Daniil Shapovalov <daniilshapovalov03@gmail.com>
2025-09-09 13:01:18 +0300

    добавлен вью и бриз

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,27 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
 
-use App\Http\Controllers\PageController;
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/about', [PageController::class, 'about']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-use App\Http\Controllers\GameController;
-
-Route::get('/games', [GameController::class, 'index'])->name('games.index');
-
-Route::get('/games/{game}', [GameController::class, 'show'])->name('games.show');
+require __DIR__.'/auth.php';
